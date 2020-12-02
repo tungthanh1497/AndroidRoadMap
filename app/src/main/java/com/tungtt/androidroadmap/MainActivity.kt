3847d178700b8deae77ca0fa2c9452f0e4aeb0d3
tungtt <tungthanh1497@gmail.com>
2020-12-02 11:33:51 +0700

    Initial commit

---
+++ app/src/main/java/com/tungtt/androidroadmap/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.tungtt.androidroadmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tungtt.androidroadmap.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/tungtt/androidroadmap/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.tungtt.androidroadmap.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
